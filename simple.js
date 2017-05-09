f8bf5924ad4606e5dcbbcb5d3f0885020552add5
Nikolai Basik <Nikolai_Basik@epam.com>
2017-05-09 18:11:02 +0300

    fix loggers

--- simple.js
+++ simple.js
@@ -2,38 +2,50 @@ const winston = require('winston'),
     WinstonCloudWatch = require('winston-cloudwatch');
 
 
-winston.loggers.add(WinstonCloudWatch, {
-    logGroupName: 'Category1',
-    logStreamName: 'Stream1',
-    awsRegion: 'us-east-1',
+winston.transports.CloudWatch1 = WinstonCloudWatch;
+winston.transports.CloudWatch2 = WinstonCloudWatch;
+
+winston.loggers.add('category1', {
+    CloudWatch1: {
+        logGroupName: 'Group1',
+        logStreamName: 'Stream1',
+        awsRegion: 'us-east-1'
+    },
     console: {
         level: 'silly',
         colorize: true,
-        label: 'Category one'
+        label: 'category one'
     }
 });
 
-winston.loggers.add(WinstonCloudWatch, {
-    logGroupName: 'Category2',
-    logStreamName: 'Stream1',
-    awsRegion: 'us-east-1',
+winston.loggers.add('category2', {
+    CloudWatch1: {
+        logGroupName: 'Group2',
+        logStreamName: 'Stream1',
+        awsRegion: 'us-east-1'
+    },
     console: {
         level: 'debug',
         colorize: true,
-        label: 'Category one'
+        label: 'category two'
     }
 });
 
-const loggerCategory1 = winston.loggers.get('Category1');
-const loggerCategory2 = winston.loggers.get('Category2');
 
-loggerCategory1.error('ERROR IN FIRST!!!');
-loggerCategory1.warn('WARNING IN FIRST!');
-loggerCategory1.info('INFO IN FIRST.');
+const logger1 = winston.loggers.get('category1');
+const logger2 = winston.loggers.get('category2');
 
-loggerCategory2.error('ERROR IN SECOND!!!');
-loggerCategory2.warn('WARNING IN SECOND!');
-loggerCategory2.info('INFO IN SECOND.');
 
-loggerCategory1.silly('SILLY...........');
-loggerCategory2.debug('DEBUG......');
+logger1.error('ERROR IN 1!!!');
+logger1.warn('WARNING IN 1!');
+logger1.info('INFO IN 1.');
+
+logger2.error('ERROR IN 2!!!');
+logger2.warn('WARNING IN 2!');
+logger2.info('INFO IN 2.');
+
+logger1.silly('SILLY IN 1...........');
+logger1.silly('DEBUG IN 1...........');
+
+logger2.silly('SILLY IN 2......');
+logger2.debug('DEBUG IN 2 ......');
