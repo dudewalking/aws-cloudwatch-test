23d3dce354e03ed81cb1e3665ab4760b8c181edf
Nikolai Basik <Nikolai_Basik@epam.com>
2017-05-09 17:45:39 +0300

    play with winston.loggers

--- simple.js
+++ simple.js
@@ -2,15 +2,38 @@ const winston = require('winston'),
     WinstonCloudWatch = require('winston-cloudwatch');
 
 
-winston.add(WinstonCloudWatch, {
-    logGroupName: 'testing',
-    logStreamName: 'first',
-    awsRegion: 'us-east-1'
+winston.loggers.add(WinstonCloudWatch, {
+    logGroupName: 'Category1',
+    logStreamName: 'Stream1',
+    awsRegion: 'us-east-1',
+    console: {
+        level: 'silly',
+        colorize: true,
+        label: 'Category one'
+    }
 });
 
-winston.error('ERROR!!!');
-winston.warn('WARNING!');
-winston.info('INFO.');
-winston.verbose('VERBOSE...');
-winston.debug('DEBUG......');
-winston.silly('SILLY...........');
+winston.loggers.add(WinstonCloudWatch, {
+    logGroupName: 'Category2',
+    logStreamName: 'Stream1',
+    awsRegion: 'us-east-1',
+    console: {
+        level: 'debug',
+        colorize: true,
+        label: 'Category one'
+    }
+});
+
+const loggerCategory1 = winston.loggers.get('Category1');
+const loggerCategory2 = winston.loggers.get('Category2');
+
+loggerCategory1.error('ERROR IN FIRST!!!');
+loggerCategory1.warn('WARNING IN FIRST!');
+loggerCategory1.info('INFO IN FIRST.');
+
+loggerCategory2.error('ERROR IN SECOND!!!');
+loggerCategory2.warn('WARNING IN SECOND!');
+loggerCategory2.info('INFO IN SECOND.');
+
+loggerCategory1.silly('SILLY...........');
+loggerCategory2.debug('DEBUG......');
