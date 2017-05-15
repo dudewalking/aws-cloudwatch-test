1d30deafdb718415cce79f502eecf6045b7c1755
Nikolai Basik <Nikolai_Basik@epam.com>
2017-05-15 17:21:14 +0300

    add another date format

--- logger.js
+++ logger.js
@@ -2,31 +2,32 @@ const winstonLogger = require('./winston-config'),
     debugLogger = winstonLogger.DebugLogger,
     infoLogger = winstonLogger.InfoLogger,
     errorLogger = winstonLogger.ErrorLogger;
+const ip = require('os').hostname();
 
 module.exports = class Logger {
 
     static logMessage(message) {
-        message = (new Date().toISOString()) + ' ' + message;
+        message = new Date().toUTCString().concat(`${ip} : ` + message);
         infoLogger.info(message);
     }
 
     static logErrorMessage(message) {
-        message = new Date().toISOString().concat(' ', message);
+        message = new Date().toUTCString().concat(`${ip} : `, message);
         errorLogger.error(message);
     }
 
     static logWarnMessage(message) {
-        message = new Date().toISOString().concat(' ', message);
+        message = new Date().toUTCString().concat(`${ip} : `, message);
         infoLogger.warn(message);
     }
 
     static logDebugMessage(message) {
-        message = new Date().toISOString().concat(' ', message);
+        message = new Date().toUTCString().concat(`${ip} : `, message);
         debugLogger.debug(message);
     }
 
     static logSillyMessage(message) {
-        message = new Date().toISOString().concat(' ', message);
+        message = new Date().toUTCString().concat(`${ip} : `, message);
         debugLogger.silly(message);
     }
 
